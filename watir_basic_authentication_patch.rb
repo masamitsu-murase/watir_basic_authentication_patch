f95d2ebb6c5cc10cb7eb2bf95b45a50fc7df4d22
Masamitsu MURASE <masamitsu.murase@gmail.com>
2011-10-15 16:58:44 +0900

    IE6 rejects longer "javascript:" than 500 characters, so XML_HTTP_REQUEST_JS_SOURCE should be compressed.

--- watir_basic_authentication_patch.rb
+++ watir_basic_authentication_patch.rb
@@ -6,28 +6,23 @@ require("watir/ie")
 module Watir
   class IE
 
-    XML_HTTP_REQUEST_JS_SOURCE = <<"EOS"
+=begin
 (function(){
-    var creators = [ function(){ return new XMLHttpRequest(); },
-                     function(){ return new ActiveXObject('Msxml2.XMLHTTP.6.0'); },
-                     function(){ return new ActiveXObject('Msxml2.XMLHTTP.3.0'); } ];
-    var xhr = null;
-    for (var i=0; i<creators.length; i++){
+    var i = 0, a = "ActiveXObject('Msxml2.XMLHTTP.", 
+        n = [ "XMLHttpRequest()", a + "6.0')", a + "3.0')" ], x, u = "URL_TEXT";
+    while(x = n[i++]){
         try{
-            xhr = creators[i]();
-            break;
-        }catch(e){
-        }
-    }
-    if (xhr){
-        xhr.onreadystatechange = function(){};
-        xhr.open("GET", "URL_TEXT", false, "USER_TEXT", "PASS_TEXT");
-        xhr.setRequestHeader("If-Modified-Since", "Thu, 01 Jun 1970 00:00:00 GMT");
-        xhr.send(null);
-        location.href = "URL_TEXT";
+            x = eval("new " + x);
+            x.open("GET", u, !1, "USER_TEXT", "PASS_TEXT");
+            x.setRequestHeader("If-Modified-Since", "Thu, 01 Jun 1970 00:00:00 GMT");
+            x.send(null);
+            location.href = u;
+        }catch(e){}
     }
 })();
-EOS
+=end
+
+    XML_HTTP_REQUEST_JS_SOURCE = '(function(){var i=0,a="ActiveXObject(\'Msxml2.XMLHTTP.",n=["XMLHttpRequest()",a+"6.0\')",a+"3.0\')"],x,u="URL_TEXT";while(x=n[i++]){try{x=eval("new "+x);x.open("GET",u,!1,"USER_TEXT","PASS_TEXT");x.setRequestHeader("If-Modified-Since","Thu, 01 Jun 1970 00:00:00 GMT");x.send(null);location.href=u}catch(e){}}})();'
 
     def goto_with_basic_authentication(url, user, pass)
       base64 = [ "#{user}:#{pass}" ].pack("m").gsub("\n", "")
@@ -35,7 +30,7 @@ EOS
       wait
       time1 = @down_load_time
 
-      js = XML_HTTP_REQUEST_JS_SOURCE.gsub(Regexp.new("\\s+"), " ")
+      js = XML_HTTP_REQUEST_JS_SOURCE
       [ [ url, "URL_TEXT" ], [ user, "USER_TEXT" ], [ pass, "PASS_TEXT" ] ].each do |item|
         escaped_str = item[0].gsub("\\"){ "\\\\" }.gsub("\""){ "\\\"" }
         js = js.gsub(item[1]){ escaped_str }
